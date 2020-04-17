85dd0e704b486f0f33fe5f7c8457fd55d443c5c5
lazaro <lazaromello14@gmail.com>
2020-04-17 17:08:39 -0300

    using promises instead of callbacks

--- Promises.js
+++ Promises.js
@@ -3,45 +3,45 @@ const utils = require('util');
 const getAdressAsync = utils.promisify(getAdress);
 
 function getUser() {
-    //Deu problema chama reject
-    //deu certo chama resolve
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            return resolve({
-                _id: 1,
-                nome: 'Jhon',
-                dataNascimento: new Date().toDateString(),
-            })
-        }, 2000);
-    })
+	//Deu problema chama reject
+	//deu certo chama resolve
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			return resolve({
+				_id: 1,
+				nome: 'Jhon',
+				dataNascimento: new Date().toDateString(),
+			})
+		}, 2000);
+	})
 }
 
 function getPhone(userId) {
 
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
 
-            return resolve({
-                telefone: 12342342,
-                ddd: 11,
+			return resolve({
+				telefone: 12342342,
+				ddd: 11,
 
-            })
+			})
 
-        }, 2000);
-    })
+		}, 2000);
+	})
 }
 
 
 function getAdress(userId, callback) {
 
-    setTimeout(() => {
+	setTimeout(() => {
 
-        return callback(undefined, {
-            logadrouro: 'Rua c',
-            numero: 293
-        })
+		return callback(undefined, {
+			logadrouro: 'Rua c',
+			numero: 293
+		})
 
-    }, 2000);
+	}, 2000);
 }
 
 //Para manipular o sucesso usamos a funcao .then
@@ -50,40 +50,47 @@ function getAdress(userId, callback) {
 const getUserPromise = getUser();
 
 getUserPromise
-    .then(user => {
-        return getPhone(user._id)
-            .then(phone => {
-                return {
-                    usuario: {
-                        nome: user.nome,
-                        _id: user._id,
-                    },
-                    telefone: phone
-                }
-            })
+	.then(user => {
+		return getPhone(user._id)
+			.then(phone => {
+				return {
+					usuario: {
+						nome: user.nome,
+						_id: user._id,
+					},
+					telefone: phone
+				}
+			})
 
-    })
-    .then((resultado) => {
-        const adress = getAdressAsync(resultado.usuario._id);
-        return adress
-            .then((result) => {
-                return {
-                    usuario: resultado.usuario,
-                    telefone: resultado.telefone,
-                    endereco: result
-                }
-            })
-    })
-    .then(user => {
-        const data = {
+	})
+	.then((resultado) => {
+		const adress = getAdressAsync(resultado.usuario._id);
+		return adress
+			.then((result) => {
+				return {
+					usuario: resultado.usuario,
+					telefone: resultado.telefone,
+					endereco: result
+				}
+			})
+	})
+	.then(user => {
+		const data = {
 
-            Nome: user.usuario.nome,
-            Endereço: `${user.endereco.logadrouro},${user.endereco.numero}`,
-            Telefone: `${(user.telefone.ddd)} ${user.telefone.telefone}`
+			Nome: user.usuario.nome,
+			Endereço: `${user.endereco.logadrouro},${user.endereco.numero}`,
+			Telefone: `${(user.telefone.ddd)} ${user.telefone.telefone}`
 
-        }
-        console.log(JSON.stringify(data));
+		}
+		console.log(JSON.stringify(data));
 
-    }).catch(err => {
-        console.log('Erro:', err)
-    })
+	}).catch(err => {
+		console.log('Erro:', err)
+	})
+
+//output
+// 	{
+// 		"Nome": "Jhon",
+// 		"Endereço": "Rua c,293",
+// 		"Telefone": "11 12342342"
+// 	}
